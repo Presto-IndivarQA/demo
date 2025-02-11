a3dbd96053de7f6fc32d16c101bcfedff1ce0554
Presto-IndivarQA <indivar.bajpai@presto.co.in>
2025-02-11 15:22:05 +0530

    Update test dot java
    add output message

--- HelloSelenium/src/hello/test.java
+++ HelloSelenium/src/hello/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello India");
+		System.out.println("Hello Selenium Check India");
 
 	}
 
